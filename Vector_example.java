1cedebd2008a79877dca86f128494a535dc12e0c
unknown <dhruvinsinh.rathod@gmail.com>
2023-05-02 14:06:31 +0530

    change comment

---
+++ Vector_example.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class ArrayList_example{
+    public static void main(String args[]){
+        //Declaration of ArrayList
+		Vector<Integer> arr=new Vector<Integer>();
+		
+		//Insert Operation
+        arr.add(10);
+        arr.add(20);
+        arr.add(40);
+        System.out.println(arr);
+        
+        //Remove work with 0-index not with values
+        arr.remove(1);
+        System.out.println(arr);
+        
+        //Update Operation
+        arr.set(0,22);
+        System.out.println(arr);
+        
+        //Read operation
+        System.out.println(arr.contains(22));
+        
+        //search Index of element
+        System.out.println(arr.indexOf(22));
+
+        //size of array
+        System.out.println(arr.size());
+        
+        //clear the array
+        arr.clear();
+        System.out.println(arr);
+    }
+
+} 
